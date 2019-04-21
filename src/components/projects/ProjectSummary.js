8c7aef65991fee021d4542b0bd7c9e57408e4b38
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-04-20 21:17:15 -0400

    initial commit

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,8 +5,8 @@ const ProjectSummary = ({project}) => {
          <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">{project.title}</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className = "grey-text">3, March, 2 am</p>
+                    <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+                    <p className = "grey-text">{project.createdAt.toDate().toString()}</p>
                 </div>
             </div>
     )
