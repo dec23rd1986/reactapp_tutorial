4a45cdefb8ee066240a290c6cc86fc64db61264f
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-04-22 22:36:09 -0400

    initial commit

---
+++ dist/precache-manifest.31abdc370c311b5b9544f3928b3bd0de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2f0ca7c90ff3f887a91",
+    "url": "/static/js/main.2aaaf180.chunk.js"
+  },
+  {
+    "revision": "291877eeb42fe81ef249",
+    "url": "/static/js/2.c3f816a6.chunk.js"
+  },
+  {
+    "revision": "a2f0ca7c90ff3f887a91",
+    "url": "/static/css/main.20820d1e.chunk.css"
+  },
+  {
+    "revision": "a451d4614b247b4008b8d7b26ac05900",
+    "url": "/index.html"
+  }
+];
